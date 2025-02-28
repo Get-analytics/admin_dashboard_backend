ff6873a806d42c65422fabc5d27e243d7b62118d
Get-analytics <team20tech@gmail.com>
2025-02-28 14:38:43 +0530

    Update app.js
    cors url configred

--- app.js
+++ app.js
@@ -9,7 +9,31 @@ const mainRouter = require("./routes/user");
 
 app.use(express.json());
 
-app.use(cors())
+
+
+
+// Middleware
+const corsOptions = {
+    origin: ['https://filescencedashboard.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+app.use((req, res, next) => {
+    const allowedOrigins = ['https://filescencedashboard.vercel.app', 'http://localhost:3000',  'http://localhost:3001'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, csrf-token');
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Welcome to the API root endpoint admin dashboard api file scence');
+});
 app.use("/api/v1", mainRouter);
 
 const port = process.env.PORT || 3000;
