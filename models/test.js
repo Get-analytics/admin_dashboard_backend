d3cf0daf4595ff9c1c65b273148d74791016542e
Get-analytics <team20tech@gmail.com>
2025-03-10 05:26:03 +0530

    Update test.js
    file name added

--- models/test.js
+++ models/test.js
@@ -4,6 +4,7 @@ const shortenedUrlSchema = new mongoose.Schema(
   {
     originalUrl: { type: String, required: true }, // Original URL or converted file URL
     shortId: { type: String, required: true },       // Shortened URL identifier
+    fileName: { type: String },
     mimeType: { type: String, required: true },      // MIME type of the file or "weblink"
     totalPages: { type: Number, default: 0 },  // Add this field to store total page count
     userUuid: { type: String, required: true },      // The UUID of the user who owns this record
