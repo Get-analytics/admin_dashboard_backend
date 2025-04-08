ea6dd2e220ed17126a01e1a792cb34e7d9137ccb
Get-analytics <team20tech@gmail.com>
2025-04-08 21:40:56 +0530

    Update test.js
    new attritube addedd

--- models/test.js
+++ models/test.js
@@ -6,8 +6,17 @@ const shortenedUrlSchema = new mongoose.Schema(
     shortId: { type: String, required: true },       // Shortened URL identifier
     fileName: { type: String },
     mimeType: { type: String, required: true },      // MIME type of the file or "weblink"
-    totalPages: { type: Number, default: 0 },  // Add this field to store total page count
-    userUuid: { type: String, required: true },      // The UUID of the user who owns this record
+    totalPages: { type: Number, default: 0 },          // Store total page count (for PDFs)
+    duration: { type: Number, default: 0 },            // Store video duration in seconds
+    userUuid: { type: String, required: true },        // The UUID of the user who owns this record
+
+    // New fields:
+    effectiveDate: { type: Date, default: Date.now },
+    expirationDate: {
+      type: Date,
+      default: () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    },
+    active: { type: String, default: "Y" },
   },
   { timestamps: true, collection: "urls" } // Use collection name "urls"
 );
