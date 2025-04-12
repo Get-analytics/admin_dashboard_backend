75e388cac9d988e35a7a03c145201d3d954eb4d0
Get-analytics <team20tech@gmail.com>
2025-04-12 21:43:38 +0530

    Update cronRoute.js
    double  route added

--- routes/cronRoute.js
+++ routes/cronRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { expireLinks } = require('../controllers/cronController');
+const { CronBasecontroller } = require('../controllers/cronController');
+const { cronInternalApi } = require('../controllers/cronInternelapi'); // fixed name
 
-router.get('/cron', expireLinks);
+router.get('/cron', CronBasecontroller);
+router.get('/cronTrigger', cronInternalApi);
 
 module.exports = router;
